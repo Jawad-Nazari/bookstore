293fc3e7211ff3fb4a1d2a8e7954d0535053dc01
Jawad <Jawad.Nazari@outlook.com>
2023-05-16 16:21:09 +0500

    Using redux selector and dispatch to display and remove book

--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -4,37 +4,14 @@ import NewBook from '../components/NewBook';
 import BooksList from '../components/BooksList';
 import Card from '../components/UI/Card';
 
-const BOOKS_DATA = [
-  {
-    id: 1,
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    category: 'Action',
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    author: 'Frank Herbert',
-    category: 'Science Fiction',
-  },
-  {
-    id: 3,
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    category: 'Economy',
-  },
-];
+const BooksPage = () => (
+  <section className="BooksPage">
+    <Card>
+      <BooksList />
+      <NewBook />
+    </Card>
+  </section>
 
-const BooksPage = () => {
-  const removeBookHandler = () => {};
-  return (
-    <section className="BooksPage">
-      <Card>
-        <BooksList onRemoveBook={removeBookHandler} books={BOOKS_DATA} />
-        <NewBook />
-      </Card>
-    </section>
-  );
-};
+);
 
 export default BooksPage;
