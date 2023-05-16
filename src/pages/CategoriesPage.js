6a61c09bf037d8fba85954fcb29195766f0871ae
Jawad <Jawad.Nazari@outlook.com>
2023-05-16 17:42:33 +0500

    Updated CategoriesPage component with Redux integration and status display

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { UPDATE_CATEGORY_STATUS } from '../redux/categories/categories';
+import Card from '../components/UI/Card';
 
-const CategoriesPage = () => <h2>Welcome to the Categories Page</h2>;
+const CategoriesPage = () => {
+  const status = useSelector((state) => state.categories.status);
+  const dispatch = useDispatch();
+
+  const checkStatusHandler = () => {
+    dispatch(UPDATE_CATEGORY_STATUS());
+  };
+
+  const statusContent = status && status !== '' ? <p>{status}</p> : null;
+
+  return (
+    <Card>
+      <h2>Welcome to the Categories Page</h2>
+      {statusContent}
+      <button type="button" onClick={checkStatusHandler}>
+        Check status
+      </button>
+    </Card>
+  );
+};
 
 export default CategoriesPage;
